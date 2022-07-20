d1e12f84e8403ff06c10e6540ded2b3aa64e1477
Omar Mirza <omar.creative.dev@gmail.com>
2022-07-20 17:01:06 +0100

    fetch movies from api, render on ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import {useEffect, useState} from 'react';
+import axios, { AxiosResponse } from 'axios';
+import { Movie, MovieResponse, MovieState } from './model/interfaces';
 
 function App() {
+  const [movies, setMovies] = useState({
+    isLoading: true,
+    data: []
+  } as MovieState);
+
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=7c97d086dce04aa54100351aa745c9f9').then((response: AxiosResponse<MovieResponse>) => {
+      const data: Movie[] = response?.data?.results;
+
+      setMovies({
+        isLoading: false,
+        data
+      })
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Movie list</h1>
+        {movies?.isLoading && `Loading...`}
+        <ul className={styles.container}>
+          {movies?.data.length > 0 && movies.data.map((movie: Movie) => {
+            return <li key={movie?.id}>{movie?.title}</li>
+          })
+          }
+        </ul>
+      </div>
   );
 }
 
